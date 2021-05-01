bd43c89e29bd455a8cf5f45fa289ae7ad2b19655
Jayrex <acilojayrex@gmail.com>
2021-05-01 13:45:04 +0800

    dynamic forminput icon

--- src/components/Auth/common/FormInput.tsx
+++ src/components/Auth/common/FormInput.tsx
@@ -2,7 +2,7 @@ import { ComponentProps } from 'react';
 import { FormGroup, FormFeedback, Input, InputGroup, InputGroupAddon, InputGroupText } from 'reactstrap';
 
 interface IFormInputProps {
-  formData: {
+  inputData: {
     inputValidation: {
       email: boolean,
       password: boolean
@@ -11,19 +11,20 @@ interface IFormInputProps {
     inputName: string,
     placeholder: string,
     inputType: ComponentProps<typeof Input>['type'],
-    errFeedback: string
+    errFeedback: string,
+    icon: string
   }
 }
 
 function FormInput(props: IFormInputProps) {
-  const { inputValidation, handleFormInput, inputName, placeholder, inputType, errFeedback } = props.formData;
+  const { inputValidation, handleFormInput, inputName, placeholder, inputType, errFeedback, icon } = props.inputData;
 
   return (
     <FormGroup className={inputValidation.email ? '' : 'has-danger' }>
       <InputGroup>
         <InputGroupAddon addonType="prepend">
           <InputGroupText>
-            <i className="ni ni-email-83" />
+            <i className={`ni ${icon}`} />
           </InputGroupText>
         </InputGroupAddon>
         <Input className={inputValidation.email ? '' : 'is-invalid' } placeholder={placeholder} type={inputType} name={inputName} onChange={(e) => handleFormInput(e.target.name, e.target.value)}/>
