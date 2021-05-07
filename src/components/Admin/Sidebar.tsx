5615f9f4a0b217772dcb43fb6d4eb6672d1ad849
Jayrex <acilojayrex@gmail.com>
2021-05-07 14:34:51 +0800

    adming dashboard initial layout and navigation items

--- src/components/Admin/Sidebar.tsx
+++ src/components/Admin/Sidebar.tsx
@@ -1,9 +1,4 @@
-import React, { useState } from "react";
-import { NavLink as NavLinkRRD, Link } from "react-router-dom";
-// nodejs library to set properties for components
-
-// reactstrap components
-import { Collapse, DropdownMenu, DropdownItem, UncontrolledDropdown, DropdownToggle, Form, Input, InputGroupAddon, InputGroupText, InputGroup, Media, NavbarBrand, Navbar, NavItem, NavLink, Nav, Container, Row, Col, } from "reactstrap"; 
+import {  Navbar, NavItem, NavLink, Nav, Container, } from "reactstrap"; 
 
 function Sidebar () {
 
@@ -19,6 +14,39 @@ function Sidebar () {
             <NavLink href="#dashboard" activeClassName="active" >
               <i className="ni ni-tv-2 text-primary" />
               Dashboard
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink href="#dashboard" activeClassName="active" >
+              <i className="fas fa-tasks text-success"></i>
+              Today
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink href="#dashboard" activeClassName="active" >
+              <i className="fas fa-calendar-alt text-orange"></i>
+              Calendar
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink href="#dashboard" activeClassName="active" >
+              <i className="fas fa-check-double text-primary"></i>
+              Notes
+            </NavLink>
+          </NavItem>
+          <hr className="my-3" />
+          {/* Heading */}
+          <h6 className="navbar-heading text-muted text-center">Projects</h6>
+          <NavItem>
+            <NavLink href="#dashboard" activeClassName="active" >
+              <i className="fas fa-plus text-muted ml-3"></i>
+              New Project
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink href="#dashboard" activeClassName="active" >
+              <i className="fas fa-trash-alt text-danger"></i>
+              Archives
             </NavLink>
           </NavItem>
         </Nav>
--- src/layouts/Admin/MainLayout.tsx
+++ src/layouts/Admin/MainLayout.tsx
@@ -1,15 +1,16 @@
-import React from "react";
-import { useLocation, Route, Switch, Redirect } from "react-router-dom";
-// reactstrap components
-import { Container } from "reactstrap";
-// core components
 import Sidebar from '../../components/Admin/Sidebar';
+import TopNavbar from '../../components/Admin/TopNavbar';
+import Header from '../../components/Admin/Header';
 
 function Admin () {
 
   return (
     <>
       <Sidebar />
+      <div className="main-content">
+        <TopNavbar />
+        <Header />
+      </div>
     </>
   );
 }
