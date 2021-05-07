70cf286a353d2b1eb626022797468e83092b5db8
Jayrex <acilojayrex@gmail.com>
2021-05-07 18:00:20 +0800

    sidebar&topbar fixed responsive markup

--- src/components/Admin/Sidebar.tsx
+++ src/components/Admin/Sidebar.tsx
@@ -1,6 +1,21 @@
-import {  Navbar, NavItem, NavLink, Nav, Container, } from "reactstrap"; 
+import { useState } from 'react';
+import {
+  Collapse,
+  Navbar,
+  NavItem,
+  NavLink,
+  Nav,
+  Container,
+  Row,
+  Col
+} from "reactstrap";
+import UserNavbar from '../../components/Admin/UserNavbar';
 
 function Sidebar () {
+  const [collapseOpen, setCollapseOpen] = useState(false);
+  const toggleCollapse =  () => {
+    setCollapseOpen(!collapseOpen);
+  };
 
   return (
     <Navbar
@@ -8,43 +23,73 @@ function Sidebar () {
       expand="md"
       id="sidenav-main"
     >
-      <Container fluid>
+      {/* Toggler */}
+      <button
+        className="navbar-toggler"
+        type="button"
+        onClick={toggleCollapse}
+      >
+        <span className="navbar-toggler-icon" />
+      </button>
+      <Nav className="align-items-center d-md-none">
+        <UserNavbar />
+      </Nav>
+
+      <Collapse navbar isOpen={collapseOpen}>
+        <div className="navbar-collapse-header d-md-none">
+          <Row>
+            <Col className="collapse-close" xs="12">
+              <button
+                className="navbar-toggler"
+                type="button"
+                onClick={toggleCollapse}
+              >
+                <span />
+                <span />
+              </button>
+            </Col>
+          </Row>
+        </div>
         <Nav navbar>
-          <NavItem>
-            <NavLink href="#dashboard" activeClassName="active" >
-              <i className="fas fa-tasks text-success"></i>
-              Today
-            </NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink href="#dashboard" activeClassName="active" >
-              <i className="fas fa-calendar-alt text-orange"></i>
-              Calendar
-            </NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink href="#dashboard" activeClassName="active" >
-              <i className="fas fa-check-double text-primary"></i>
-              Notes
-            </NavLink>
-          </NavItem>
-          <hr className="my-3" />
-          {/* Heading */}
-          <h6 className="navbar-heading text-muted text-center">Projects</h6>
-          <NavItem>
-            <NavLink href="#dashboard" activeClassName="active" >
-              <i className="fas fa-plus text-muted ml-3"></i>
-              New Project
-            </NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink href="#dashboard" activeClassName="active" >
-              <i className="fas fa-trash-alt text-danger"></i>
-              Archives
-            </NavLink>
-          </NavItem>
+          <Container fluid>
+            <Nav navbar>
+              <NavItem>
+                <NavLink href="#dashboard" activeClassName="active" >
+                  <i className="fas fa-tasks text-success"></i>
+                  Today
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="#dashboard" activeClassName="active" >
+                  <i className="fas fa-calendar-alt text-orange"></i>
+                  Calendar
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="#dashboard" activeClassName="active" >
+                  <i className="fas fa-check-double text-primary"></i>
+                  Notes
+                </NavLink>
+              </NavItem>
+              <hr className="my-3" />
+              {/* Heading */}
+              <h6 className="navbar-heading text-muted text-center">Projects</h6>
+              <NavItem>
+                <NavLink href="#dashboard" activeClassName="active" >
+                  <i className="fas fa-plus text-muted ml-3"></i>
+                  New Project
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="#dashboard" activeClassName="active" >
+                  <i className="fas fa-trash-alt text-danger"></i>
+                  Archives
+                </NavLink>
+              </NavItem>
+            </Nav>
+          </Container>
         </Nav>
-      </Container>
+      </Collapse>
     </Navbar>
   );
 }
---
+++ src/components/Admin/UserNavbar.tsx
@@ -0,0 +1,77 @@
+import { Link } from "react-router-dom";
+import {
+  DropdownMenu,
+  DropdownItem,
+  UncontrolledDropdown,
+  DropdownToggle,
+  Media
+} from "reactstrap";
+
+import jess from '../../assets/img/theme/team-4-800x800.jpg';
+
+function UserNavbar () {
+  return (
+    <>
+      {/**<UncontrolledDropdown nav>
+        <DropdownToggle nav className="nav-link-icon">
+          <i className="ni ni-bell-55" />
+        </DropdownToggle>
+        <DropdownMenu
+          aria-labelledby="navbar-default_dropdown_1"
+          className="dropdown-menu-arrow"
+          right
+        >
+          <DropdownItem>Action</DropdownItem>
+          <DropdownItem>Another action</DropdownItem>
+          <DropdownItem divider />
+          <DropdownItem>Something else here</DropdownItem>
+        </DropdownMenu>
+      </UncontrolledDropdown>**/}
+      <UncontrolledDropdown nav>
+        <DropdownToggle className="pr-0" nav>
+          <Media className="align-items-center">
+            <span className="avatar avatar-sm rounded-circle">
+              <img
+                alt="..."
+                src={jess}
+              />
+            </span>
+            <Media className="ml-2 d-none d-lg-block">
+              <span className="mb-0 text-sm font-weight-bold">
+                Jessica Jones
+              </span>
+            </Media>
+          </Media>
+        </DropdownToggle>
+        <DropdownMenu className="dropdown-menu-arrow" right>
+          <DropdownItem className="noti-title" header tag="div">
+            <h6 className="text-overflow m-0">Welcome!</h6>
+          </DropdownItem>
+          <DropdownItem to="/admin/user-profile" tag={Link}>
+            <i className="ni ni-single-02" />
+            <span>My profile</span>
+          </DropdownItem>
+          <DropdownItem to="/admin/user-profile" tag={Link}>
+            <i className="ni ni-settings-gear-65" />
+            <span>Settings</span>
+          </DropdownItem>
+          <DropdownItem to="/admin/user-profile" tag={Link}>
+            <i className="ni ni-calendar-grid-58" />
+            <span>Activity</span>
+          </DropdownItem>
+          <DropdownItem to="/admin/user-profile" tag={Link}>
+            <i className="ni ni-support-16" />
+            <span>Support</span>
+          </DropdownItem>
+          <DropdownItem divider />
+          <DropdownItem href="#pablo" onClick={e => e.preventDefault()}>
+            <i className="ni ni-user-run" />
+            <span>Logout</span>
+          </DropdownItem>
+        </DropdownMenu>
+      </UncontrolledDropdown>
+    </>
+  );
+}
+
+export default UserNavbar;
