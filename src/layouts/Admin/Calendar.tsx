6ad97601eefd8dc0735a8eadcb1327ba35948647
Jayrex <acilojayrex@gmail.com>
2021-05-08 21:50:24 +0800

    basic working big calendar

--- src/layouts/Admin/Calendar.tsx
+++ src/layouts/Admin/Calendar.tsx
@@ -1,21 +1,48 @@
-import React from 'react'
-import { Container } from 'reactstrap';
-import FullCalendar from '@fullcalendar/react'
-import dayGridPlugin from '@fullcalendar/daygrid'
-import timeGridPlugin from '@fullcalendar/timegrid';
-import listPlugin from '@fullcalendar/list';
+import React, {useState} from 'react'
+import { Container, Row, Col } from 'reactstrap';
+import { Calendar, momentLocalizer, Views } from 'react-big-calendar'
+import moment from 'moment'
 
 import Header from '../../components/Admin/Header';
+import events from './events';
 
-export default function Calendar () {
+type DescribeFn = {
+  start: any;
+  end: any;
+}
+
+export default function CalendarC () {
+  const [eventsList, setEvents] = useState(events);
+  const localizer = momentLocalizer(moment);
+  const handleSelect = (fn: DescribeFn) => {
+    const title = window.prompt('New Event name')
+    const id = eventsList.length + 1;
+    setEvents([...eventsList,
+      {
+        start: fn.start,
+        end: fn.end,
+        id: id,
+        title: 'All Day Event very long title'
+      } ]);
+  }
+
   return (
     <>
-      <Header />
-      <Container className="mt--7" fluid>
-        <FullCalendar
-          plugins={[ dayGridPlugin ]}
-          initialView="dayGridMonth"
-        />
+      <div className="header bg-gradient-info pb-2 pt-5 pt-md-8">
+      </div>
+      <Container fluid>
+        <Row>
+          <Col>
+            <Calendar
+              selectable
+              localizer={localizer}
+              events={eventsList}
+              defaultView='month'
+              onSelectEvent={event => alert(event.title)}
+              onSelectSlot={handleSelect}
+            />
+          </Col>
+        </Row>
       </Container>
     </>
   )
